f15174dcf365185200aea72939d0e70cd9503d0e
Grace <gqxxnn@gmail.com>
2021-11-27 16:07:35 +0000

    binary seacrh

--- Main.java
+++ Main.java
@@ -5,6 +5,25 @@ import java.util.HashSet;
 public class Main {
 
     public static void main(String[] args) {
+        search(new int[]{5}, 5);
+    }
+
+    /**
+     * Approach: Binary Search
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
     }
 
      //Kadane's Algorithms - create a subarray with the largest sum
